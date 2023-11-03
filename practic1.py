dc08e8901ad65c44bf76a1ce5ca0c8547a5bd6d7
PavelBaygozin <149809130+PavelBaygozin@users.noreply.github.com>
2023-11-03 19:07:30 +0300

    Update practic1.py

--- practic1.py
+++ practic1.py
@@ -1,4 +1,3 @@
 from transformers import pipeline
 model = pipeline(model="seara/rubert-tiny2-ru-go-emotions")
-model("Привет, ты мне нравишься!")
-# [{'label': 'love', 'score': 0.5955629944801331}]
+model("Мне нравится машинное обуччение")
