9c330c25bdc3e45d60a41b987817ed50aa4d749e
dmitryloschinin <loschinin@list.ru>
2023-11-05 21:31:20 +0300

    Chat-bot

---
+++ chat.py
@@ -0,0 +1,40 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# import nltk
+
+# Necessary to download components once before usage
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('wordnet')
+
+# Loading pre-trained model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-350M-mono")
+tokenizer.padding_side = 'left'  # Set padding_side after initializing the tokenizer
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
+# Function for chatbot with text pre-check
+def chatbot_response(input_text):
+    # Encoding the input text and adding special tokens
+    input_ids = tokenizer.encode(input_text + tokenizer.eos_token + "", return_tensors='pt')
+
+    # Generating model response
+    chat_history_ids = model.generate(input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+    # Decoding the response and returning it
+    response = tokenizer.decode(chat_history_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
+    return response
+
+
+# Chat with the chatbot
+print("Chat-bot activated. Write something to start the conversation (type 'exit' to finish)")
+while True:
+    # Receiving input from the user
+    user_input = input("You: ")
+    if user_input.lower() == 'exit':
+        print("Chat-bot deactivated.")
+        break
+
+    # Receiving and displaying chatbot response
+    response = chatbot_response(user_input)
+    print("Chat-bot: ", response)
