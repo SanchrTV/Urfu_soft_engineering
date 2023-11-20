451398ff41287ba2b74f49aaa10ffa81a80a20a4
PavelBaygozin <pavel_b@inbox.ru>
2023-11-20 23:56:44 +0300

    update requirements.txt

--- fast_api.py
+++ fast_api.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+from app import tokenizer, model
 
 app = FastAPI()
 
+
 class ChatRequest(BaseModel):
     user_input: str
+
 
 # @app.post("/chat/")
 # async def chat(chat_request: ChatRequest):
@@ -14,11 +17,11 @@ class ChatRequest(BaseModel):
 
 @app.post("/chat/")
 async def chat(chat_request: ChatRequest):
-    #необходимо импортировать tokenizer и model из другого файла в репозитории
+    # необходимо импортировать tokenizer и model из другого файла в репозитории
     input_ids = tokenizer.encode(chat_request.user_input + tokenizer.eos_token, return_tensors='pt')
     chat_history_ids = model.generate(input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
     response = tokenizer.decode(chat_history_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
     return {"response": response}
 
-#для запуска в терминале: 
+# для запуска в терминале:
 # uvicorn main:app --reload
