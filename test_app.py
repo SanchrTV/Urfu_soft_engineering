bc290cce16c3ab72ef7960f4973794947828e877
dmitryloschinin <loschinin@list.ru>
2023-12-13 00:17:03 +0300

    adding tests

---
+++ test_app.py
@@ -0,0 +1,10 @@
+# test_app.py
+
+import pytest
+from app import chatbot_response
+
+def test_chatbot_response():
+    user_input = "Hello! How are you?"
+    actual_response = chatbot_response(user_input)
+
+    assert actual_response != "", "The response should not be empty."
---
+++ test_fast_app.py
@@ -0,0 +1,22 @@
+# test_fast_api.py
+
+from fastapi.testclient import TestClient
+from fast_api import app
+
+client = TestClient(app)
+
+def test_chat():
+    # Send a test request to the /chat/ endpoint
+    response = client.post(
+        "/chat/",
+        json={"user_input": "Hello! How are you?"}
+    )
+
+    # Check that a response was received
+    assert response.status_code == 200
+
+    # Check that the response contains the 'response' key
+    assert "response" in response.json()
+
+    # Check that the 'response' key in the response is not an empty string
+    assert response.json()["response"], "The response should not be empty."
