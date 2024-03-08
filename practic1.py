44b51b99f2ab4e28e8f177b1fba9c794cbb71923
SanchrTV <92468853+SanchrTV@users.noreply.github.com>
2024-03-08 14:45:47 +0300

    remove files

--- practic1.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-model = pipeline("sentiment-analysis",
-                 "blanchefort/rubert-base-cased-sentiment")
-print(model("Мне нравится машинное обучение"))
