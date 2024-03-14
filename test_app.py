6f9f683e9b0a428072647fb2c81c3035e0b32502
SanchrTV <92468853+SanchrTV@users.noreply.github.com>
2024-03-14 21:48:38 +0300

    Added 2 tests

---
+++ test_app.py
@@ -0,0 +1,7 @@
+from app import chatbot_response
+
+
+def test_chatbot_response():
+    user_input = "Hello! How are you?"
+    actual_response = chatbot_response(user_input)
+    assert actual_response != "", "The response should not be empty"
